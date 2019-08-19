bc23a255dda0e288c972c40055dd7525c1a6544a
Lubomir Peikov <l.peikov@webfashion.bg>
2019-08-19 10:55:35 +0300

    add: Added all planed components to the library

--- projects/foundry-sdk/src/lib/foundry-sdk.module.ts
+++ projects/foundry-sdk/src/lib/foundry-sdk.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
 import { WorkspaceZoneComponent } from './components/workspace-zone/workspace-zone.component';
 import { WorkspaceWrapperComponent } from './components/workspace-wrapper/workspace-wrapper.component';
+import { AccordionComponent } from './components/accordion/accordion.component';
+import { ListComponent } from './components/list/list.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { TabsComponent } from './components/tabs/tabs.component';
+import { TreeComponent } from './components/tree/tree.component';
 
 
 @NgModule({
     declarations: [
         WorkspaceZoneComponent,
         WorkspaceWrapperComponent,
+        AccordionComponent,
+        ListComponent,
+        NavigationComponent,
+        TabsComponent,
+        TreeComponent,
     ],
     imports: [
     ],
     exports: [
         WorkspaceZoneComponent,
         WorkspaceWrapperComponent,
+        AccordionComponent,
+        ListComponent,
+        NavigationComponent,
+        TabsComponent,
+        TreeComponent,
     ]
 })
 export class FoundrySdkModule { }
